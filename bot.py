a0b310175269928f5fd7efd8ac5d78ab33bcfe05
Hal Jarrett <hjarrett3@gatech.edu>
2018-01-29 20:31:43 -0500

    added custom functionality

--- bot.py
+++ bot.py
@@ -2,30 +2,49 @@ import os
 import random
 from flask import Flask, json, request
 import requests
+import re
 
 
-		app = Flask(__name__)
+        app = Flask(__name__)
 
 def reply(message):
-	payload = {
-		'bot_id' : os.environ['BOT_ID'],
-		'text'   : message,
-	}
-	requests.post('https://api.groupme.com/v3/bots/post', json=payload)
+    payload = {
+        'bot_id' : os.environ['BOT_ID'],
+        'text'   : message,
+    }
+    requests.post('https://api.groupme.com/v3/bots/post', json=payload)
 
 
 @app.route('/', methods=['POST'])
 def groupme_callback():
-	json_body = request.get_json()
-	if json_body['group_id'] == os.environ['GROUP_ID'] and json_body['sender_type'] != 'bot':
-		# some degree of verification that it is sent via a groupme callback
-		# could also check for "User-Agent: GroupMeBotNotifier/1.0", but that's plenty spoofable
+    json_body = request.get_json()
+    if json_body['group_id'] == os.environ['GROUP_ID'] and json_body['sender_type'] != 'bot':
+        # some degree of verification that it is sent via a groupme callback
+        # could also check for "User-Agent: GroupMeBotNotifier/1.0", but that's plenty spoofable
 
-		message = json_body['text']
-		### BOT CODE GOES HERE! ###
+        message = json_body['text']
+        ### BOT CODE GOES HERE! ###
+        if re.compile("^/oddsbot [0-9]+$").match(message):
+                (_, maxval)
+                r1 = random.randrange(1,maxval)
+                r2 = random.randrange(1,maxval)
+                r3 = random.randrange(1,maxval)
+                r4 = random.randrange(1,maxval)
+                response = "3, 2, 1...{}! {}!".format(r1,r2)
+                if r1 == r2:
+                        #case where you lose
+                        response += "\nYou Lose!"
+                else:
+                        response += "\nThrowback: 3, 2, 1...{}! {}!".format(r3,r4)
+                        if r3 == r4:
+                                #case where they lose
+                                response += "\nThey Lose!"
+                        else:
+                                response += "\nPhew!"
+                reply(response)
 
 
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
-	# app.run(host='0.0.0.0', port=port, debug=True)
-	app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    # app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
