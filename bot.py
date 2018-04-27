877f6da29c93c569a2b96f2ed10ac031fb2e6294
Hal Jarrett <hjarrett3@gatech.edu>
2018-04-27 11:37:32 -0400

    WIP

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ def groupme_callback():
         # could also check for "User-Agent: GroupMeBotNotifier/1.0", but that's plenty spoofable
 
         message = json_body['text']
-        if "adam" in message.lower().split():
+        if any(adam in message.lower().split() for adam in ["adam", "@adam"]:
                 print("Adam found!")
                 reply("Adam!")
         else:
